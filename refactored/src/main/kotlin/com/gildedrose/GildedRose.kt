c06adaba1f1ad62f026ea8c20e414f2d4a5c5e25
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-15 15:13:45 +0100

    Refactor on GildedRose: change for loop by forEach idiomatic kotlin

--- refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -12,51 +12,51 @@ class GildedRose(private val items: List<Item>) {
     }
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != AGED_BRIE_ITEM_NAME && items[i].name != BACKSTAGE_PASSES_ITEM_NAME) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != SULFURAS_ITEM_NAME) {
-                        items[i].quality -= 1
+        items.forEach {  item ->
+            if (item.name != AGED_BRIE_ITEM_NAME && item.name != BACKSTAGE_PASSES_ITEM_NAME) {
+                if (item.quality > 0) {
+                    if (item.name != SULFURAS_ITEM_NAME) {
+                        item.quality -= 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality += 1
+                if (item.quality < 50) {
+                    item.quality += 1
 
-                    if (items[i].name == BACKSTAGE_PASSES_ITEM_NAME) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
+                    if (item.name == BACKSTAGE_PASSES_ITEM_NAME) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality += 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality += 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != SULFURAS_ITEM_NAME) {
-                items[i].sellIn -= 1
+            if (item.name != SULFURAS_ITEM_NAME) {
+                item.sellIn -= 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != AGED_BRIE_ITEM_NAME) {
-                    if (items[i].name != BACKSTAGE_PASSES_ITEM_NAME) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != SULFURAS_ITEM_NAME) {
-                                items[i].quality -= 1
+            if (item.sellIn < 0) {
+                if (item.name != AGED_BRIE_ITEM_NAME) {
+                    if (item.name != BACKSTAGE_PASSES_ITEM_NAME) {
+                        if (item.quality > 0) {
+                            if (item.name != SULFURAS_ITEM_NAME) {
+                                item.quality -= 1
                             }
                         }
                     } else {
-                        items[i].quality -= items[i].quality
+                        item.quality -= item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1
+                    if (item.quality < 50) {
+                        item.quality += 1
                     }
                 }
             }
