542e48512ae84776675e46b9f148fc0887d316e7
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-18 09:59:16 +0100

    Refactor on ItemAger: extract age logic in different aging methods

--- refactored/src/main/kotlin/com/gildedrose/ItemAger.kt
+++ refactored/src/main/kotlin/com/gildedrose/ItemAger.kt
@@ -6,32 +6,33 @@ class ItemAger {
 
     companion object {
 
-        private const val AGED_BRIE_ITEM_NAME = "Aged Brie"
-
-        private const val SULFURAS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
-
-        private const val BACKSTAGE_PASSES_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert"
-
+        private const val AGED_BRIE = "Aged Brie"
+        private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+        private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
         private const val MIN_QUALITY = 0
-
         private const val MAX_QUALITY = 50
     }
 
-    fun ageOneDay(item: Item): AgedItem {
-        if (item.isLegendary()) return AgedItem(item.name, item.sellIn, item.quality)
-        val newQuality = when (item.name) {
-            AGED_BRIE_ITEM_NAME -> if (item.sellIn <= 0) item.quality + 2 else item.quality.inc()
-            BACKSTAGE_PASSES_ITEM_NAME ->
-                if (item.sellIn <= 0) 0
-                else if (item.sellIn <= 5) item.quality + 3
-                else if (item.sellIn <= 10) item.quality + 2
-                else item.quality.inc()
-
-            else -> if (item.sellIn <= 0) item.quality - 2 else item.quality.dec()
-        }.coerceIn(MIN_QUALITY, MAX_QUALITY)
-        val newSellIn = item.sellIn.dec()
-        return AgedItem(item.name, newSellIn, newQuality)
+    fun ageOneDay(item: Item): AgedItem = when (item.name) {
+        SULFURAS -> AgedItem(item.name, item.sellIn, item.quality)
+        AGED_BRIE -> ageBrie(item)
+        BACKSTAGE_PASSES -> ageBackstagePasses(item)
+        else -> ageStandardItem(item)
     }
 
-    private fun Item.isLegendary() = this.name == SULFURAS_ITEM_NAME
+    private fun ageBrie(item: Item) = (if (item.sellIn <= 0) item.quality + 2 else item.quality.inc())
+        .coerceIn(MIN_QUALITY, MAX_QUALITY)
+        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+
+    private fun ageBackstagePasses(item: Item) = when {
+        item.sellIn <= 0 -> 0
+        item.sellIn <= 5 -> item.quality + 3
+        item.sellIn <= 10 -> item.quality + 2
+        else -> item.quality.inc()
+    }.coerceIn(MIN_QUALITY, MAX_QUALITY)
+        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+
+    private fun ageStandardItem(item: Item) = (if (item.sellIn <= 0) item.quality - 2 else item.quality.dec())
+        .coerceIn(MIN_QUALITY, MAX_QUALITY)
+        .let { AgedItem(item.name, item.sellIn.dec(), it) }
 }
