01ba1b52ffab8e8dba9ba7a43931261380803a09
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-18 07:03:21 +0100

    Refactor on GildedRose: Remove remaining legacy code

--- refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,27 +2,11 @@ package com.gildedrose
 
 class GildedRose(private val items: List<Item>, private val itemAger: ItemAger = ItemAger()) {
 
-    companion object {
-
-        private const val AGED_BRIE_ITEM_NAME = "Aged Brie"
-
-    }
-
     fun updateQuality() {
         items.forEach { item ->
             val agedItem = itemAger.ageOneDay(item)
             item.quality = agedItem.quality
             item.sellIn = agedItem.sellIn
-
-            if (item.sellIn < 0) {
-                if (item.name == AGED_BRIE_ITEM_NAME) {
-                    if (item.quality < 50) {
-                        item.quality += 1
-                    }
-                }
-            }
         }
     }
-
 }
-
