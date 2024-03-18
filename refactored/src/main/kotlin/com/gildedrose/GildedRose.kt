77825ee43eaead5095c655e17c83987a128f499b
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-18 06:58:20 +0100

    Refactor on GildedRose: Integrate ItemAger new code for degrading quality twice

--- refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ refactored/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,11 +4,8 @@ class GildedRose(private val items: List<Item>, private val itemAger: ItemAger =
 
     companion object {
 
-        private const val BACKSTAGE_PASSES_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert"
-
         private const val AGED_BRIE_ITEM_NAME = "Aged Brie"
 
-        private const val SULFURAS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
     }
 
     fun updateQuality() {
@@ -18,15 +15,7 @@ class GildedRose(private val items: List<Item>, private val itemAger: ItemAger =
             item.sellIn = agedItem.sellIn
 
             if (item.sellIn < 0) {
-                if (item.name != AGED_BRIE_ITEM_NAME) {
-                    if (item.name != BACKSTAGE_PASSES_ITEM_NAME) {
-                        if (item.quality > 0) {
-                            if (item.name != SULFURAS_ITEM_NAME) {
-                                item.quality -= 1
-                            }
-                        }
-                    }
-                } else {
+                if (item.name == AGED_BRIE_ITEM_NAME) {
                     if (item.quality < 50) {
                         item.quality += 1
                     }
