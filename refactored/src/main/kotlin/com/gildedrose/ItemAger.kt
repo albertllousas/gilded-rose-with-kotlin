bcfdba5965265ce89312f7ebfd6c214e405aa577
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-18 13:06:46 +0100

    Refactor on ItemAger: introduce 'ageItem' generic function

--- refactored/src/main/kotlin/com/gildedrose/ItemAger.kt
+++ refactored/src/main/kotlin/com/gildedrose/ItemAger.kt
@@ -24,23 +24,22 @@ class ItemAger(private val minQuality: Int = MIN_QUALITY, private val maxQuality
 
     private fun ageLegendaryItem(item: Item) = AgedItem(item.name, item.sellIn, item.quality)
 
-    private fun ageBrie(item: Item) = (if (item.sellIn <= 0) item.quality + 2 else item.quality.inc())
-        .coerceIn(minQuality, maxQuality)
-        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+    private fun ageBrie(item: Item) = ageItem(item, whenExpired = { it.quality + 2 }, otherwise = { it.quality.inc() })
 
-    private fun ageBackstagePasses(item: Item) = when {
-        item.sellIn <= 0 -> 0
-        item.sellIn <= 5 -> item.quality + 3
-        item.sellIn <= 10 -> item.quality + 2
-        else -> item.quality.inc()
-    }.coerceIn(minQuality, maxQuality)
-        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+    private fun ageBackstagePasses(item: Item) = ageItem(
+        item,
+        whenExpired = { 0 },
+        otherwise = { if (item.sellIn <= 5) item.quality + 3 else if (item.sellIn <= 10) item.quality + 2 else item.quality + 1 }
+    )
 
-    private fun ageStandardItem(item: Item) = (if (item.sellIn <= 0) item.quality - 2 else item.quality.dec())
-        .coerceIn(minQuality, maxQuality)
-        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+    private fun ageStandardItem(item: Item) =
+        ageItem(item, whenExpired = { it.quality - 2 }, otherwise = { it.quality - 1 })
 
-    private fun ageConjuredItem(item: Item) = (if (item.sellIn <= 0) item.quality -4 else item.quality - 2)
-        .coerceIn(minQuality, maxQuality)
-        .let { AgedItem(item.name, item.sellIn.dec(), it) }
+    private fun ageConjuredItem(item: Item) =
+        ageItem(item, whenExpired = { it.quality - 4 }, otherwise = { it.quality - 2 })
+
+    private fun ageItem(item: Item, whenExpired: (Item) -> Int, otherwise: (Item) -> Int) =
+        (if (item.sellIn <= 0) whenExpired(item) else otherwise(item))
+            .coerceIn(minQuality, maxQuality)
+            .let { AgedItem(item.name, item.sellIn.dec(), it) }
 }
